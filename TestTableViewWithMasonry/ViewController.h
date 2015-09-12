16186d1d6fbb207744d6bc8a2dca93c92f8ba7bb
858665536 <shanghai@192.168.2.104>
2015-09-12 20:17:17 +0800

    Initial Commit

---
+++ TestTableViewWithMasonry/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestTableViewWithMasonry
+//
+//  Created by ShangHai on 15/9/12.
+//  Copyright (c) 2015年 yy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestTableViewWithMasonryTests/TestTableViewWithMasonryTests.m
@@ -0,0 +1,40 @@
+//
+//  TestTableViewWithMasonryTests.m
+//  TestTableViewWithMasonryTests
+//
+//  Created by ShangHai on 15/9/12.
+//  Copyright (c) 2015年 yy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface TestTableViewWithMasonryTests : XCTestCase
+
+@end
+
+@implementation TestTableViewWithMasonryTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
